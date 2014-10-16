f4cb6123bc8ecd11014724939027dd592f79238f
Evan Sagge <evansagge@gmail.com>
2014-10-16 02:06:43 -0400

    Initialized User list (closes #1)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create default manager
+manager = User.find_or_create_by!(name: 'Manager')
+manager.update!(roles: %w(manager))
+
+# Seed users
+%w(Sherry Boris Vicente Matte Jack Kevin Zoe Jay Eadon Franky Luis James).each do |name|
+  user = User.find_or_create_by!(name: name)
+  user.update!(roles: %w(support))
+end
