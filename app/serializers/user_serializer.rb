f4df2c6d2f5cde57cd312afbb785d9e476aea0cc
“Evan <evansagge@gmail.com>
2014-10-22 03:03:10 -0400

    Removed user-scoped route for support schedules, pass :user_id as a URL paramter instead

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,9 +1,3 @@
 class UserSerializer < ApplicationSerializer
-  attributes :id, :name, :links
-
-  def links
-    {
-      support_schedules: v1_user_support_schedules_url(object)
-    }
-  end
+  attributes :id, :name
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   namespace :v1 do
     resources :support_schedules, except: %i(new edit)
-    resources :users, except: %i(new edit) do
-      resources :support_schedules, only: :index
-    end
+    resources :users, except: %i(new edit)
   end
 end
